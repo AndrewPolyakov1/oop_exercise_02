90b38967b860d0df7d6ac7ac297d7045bf37287f
Andrew <mr.dijoy@gmail.com>
2020-10-01 21:28:24 +0300

    Added method for bringing to 0-360, added user literal

--- Angle.hpp
+++ Angle.hpp
@@ -1,5 +1,5 @@
 #pragma once
-
+#include <string>
 class Angle
 {
 private:
@@ -8,14 +8,18 @@ private:
 public:
     Angle(){}
     ~Angle(){}
-
+    Angle(int Deg, int Min)
+    {
+        this->degree = Deg;
+        this->minute = Min;
+    }
     void setAn(int Deg, int Min)
     {
-        degree = Deg;
-        minute = Min;
+        this->degree = Deg;
+        this->minute = Min;
     }
 
-    int GetDeg()
+    int getDeg()
     {
         return degree;
     }
@@ -24,5 +28,49 @@ public:
     {
         return minute;
     }
+
+    void Reduce()
+    {
+        int state = 0;
+        do{
+        state = 0;
+        if(this->degree >= 360)
+        {
+            this->degree -= 360;
+            state ++;
+        }
+        if(this->degree <= 0)
+        {
+            this->degree += 360;
+            state ++;
+        }
+        if(this->minute >= 60)
+        {
+            this->minute -= 60;
+            this->degree += 1;
+            state ++;
+        }
+        }while(state > 0);
+    }
 };
 
+Angle operator ""_angle(const char* angle)
+{
+    Angle phi;
+    int state = 0;
+    std::string tmp_deg, tmp_min;
+    for(int i = 0; i < strlen(angle); i++)
+    {
+        if ((angle[i] == '.') || (angle[i] == ',')) state = 1;
+        if((state == 0) && !((angle[i] == '.') || (angle[i] == ',')))
+        {
+            tmp_deg += angle[i];
+        }
+        if((state == 1) && !((angle[i] == '.') || (angle[i] == ',')))
+        {
+            tmp_min += angle[i];
+        }
+    }
+    phi.setAn(std::stoi(tmp_deg,nullptr,10),std::stoi(tmp_min,nullptr,10));
+    return phi;
+}
--- main.cpp
+++ main.cpp
@@ -2,11 +2,18 @@
 //M_PI
 #include <iostream>
 #include <cmath>
+#include <string>
+#include <cstring>
 #include "Angle.hpp"
 
 
 int main()
 {
-
+    Angle alpha;
+    alpha = 721.121_angle;
+    alpha.setAn(-365, 500);
+    std::cout << alpha.getDeg() << "\'" <<alpha.getMin() << "\'\'" << std::endl;
+    alpha.Reduce();
+    std::cout << "#" << alpha.getDeg() << "\'" <<alpha.getMin() << "\'\'" << std::endl;
     return 0;
 }
